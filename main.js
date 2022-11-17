25b52f4fcd02f5cd9067a3ca52d1a70b1f4fa651
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2022-11-17 11:17:46 -0600

    made some new color mides

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+.better-Dmode {
+  background-color: #1f2224;
+  color: white;
+}
+.light-mode {
+  background-color: white;
+  color: black;
+}
+.blue-mode {
+  background-color: #3a9dd6;
+  color: white;
+}
