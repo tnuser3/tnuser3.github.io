5a05536b28b5e00d511cae977237d61accbf3222
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2023-02-08 08:34:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-function BlueMode() {
-  var element = document.body;
-  element.classList.toggle("Blue-mode");
-}
-function DarkMode() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-   var col=document.getElementById("H1");
-col.style.color="#FF0000";
-}
-function Menustart() {
-  var doc = document;
-  var img = doc.getElementById("Image")
-  img.style.visibility = visible;
-}
+
 function Comingsoon(){
   alert('COMING SOON')
 }
