817e4bd970a060ce2a2c7acf72cf198eeb77be55
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2023-02-17 08:24:52 -0600

    Update Webho.js

--- Javascript/Webho.js
+++ Javascript/Webho.js
@@ -1,1 +1,23 @@
 
+    function sendMessage() {
+	    var URI;
+      var Text;
+      var UserName;
+      var IconURI
+      URI= document.getElementById("URIArea").value;
+      Text = document.getElementById("TextArea").value;
+      UserName document.getElementById("UsernameArea").value;
+      IconURI = document.getElementById("IconUrl").value;
+      const request = new XMLHttpRequest();
+      request.open("POST", URI);
+
+      request.setRequestHeader('Content-type', 'application/json');
+
+      const params = {
+        username: UserName,
+        avatar_url: "",
+        content: Text
+      }
+
+      request.send(JSON.stringify(params));
+    }
