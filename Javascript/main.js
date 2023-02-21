9c76ae55ebe427b7c27de1c436b2932422f1f414
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2023-02-21 14:35:27 -0600

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -2,28 +2,7 @@
 function Comingsoon(){
   alert('COMING SOON')
 }
-function Closemain(){
-  var doc=document;
-  doc.getElementById("H").style.visibility="visible";
-  doc.getElementById("Links").style.visibility="visible";
-  doc.getElementById("P3").style.visibility="visible";
-  doc.getElementById("MHN").style.visibility= "visible";
-  doc.getElementById("discord").style.visibility="visible";
-  doc.getElementById("Back").style.visibility="hidden";
-  doc.getElementById("line").style.visibility="hidden"
-  doc.getElementById("Tracker-Main").style.visibility="hidden";
-}
-function Openmain(){
-var doc=document;
-doc.getElementById("Back").style.visibility="visible";
-doc.getElementById("H").style.visibility="hidden";
-doc.getElementById("Links").style.visibility="hidden";
-doc.getElementById("P3").style.visibility="hidden";
-doc.getElementById("MHN").style.visibility= "hidden";
-doc.getElementById("discord").style.visibility="hidden";
-doc.getElementById("line").style.visibility="visible";
-doc.getElementById("Tracker-Main").style.visibility="visible";
-}
+
 	  window.onload = function() {
  	  OnStart();
 	  };
