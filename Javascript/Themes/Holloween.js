00b7717d6549257e7b1bcb78eb1a3eac9a922b3f
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2023-02-28 14:23:06 -0600

    da da d a

--- Javascript/Themes/Holloween.js
+++ Javascript/Themes/Holloween.js
@@ -1,4 +1,18 @@
 var doc = document;
-function HeaderFont(){
-    doc.getElementById("H").style.fontFamily="CustomFont";
+window.onload = function() {
+    CheckEndDate();
+   };
+function CheckEndDate() {
+
+    var now = new Date().toString();  // current date
+    var end_date = new Date("February 28").toString();
+
+    if (now === end_date) {
+        doc.getElementById("H").style.fontFamily="CustomFont";
+        alert('HAPPY HALLOWEEN 🎃');
+        return;
+    }
+    else{
+        console.log("Not HAlloween ):")
+    }
 }
