3909004d6884114a97deb283527ff6e5c6e3f57a
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2023-03-02 20:35:50 -0600

    Update Themes.js

--- Javascript/Themes.js
+++ Javascript/Themes.js
@@ -8,7 +8,7 @@ function Start(){
     const d = new Date();
     
     //-------------------christmas-------------------\\
-        if(d.getMonth() == 2){
+        if(d.getMonth() == 11){
             console.log("Happy Christmas ❄");
             alert("Merry Christmas ❄");
             document.getElementById("snowflakes").style.visibility = "Visible";
@@ -17,7 +17,7 @@ function Start(){
             document.getElementById("snowflakes").style.visibility = "hidden";
         }
         //-------------------Holloween-------------------\\
-        if(d.getMonth() == 2){
+        if(d.getMonth() == 9){
             console.log("Happy Halloween 🎃");
              alert("Happy Halloween 🎃");
             document.getElementById("H").style.fontFamily="CustomFont";
@@ -28,8 +28,10 @@ function Start(){
             console.log("not Halloween ):");
         }
         //-------------------4th of july-------------------\\
-        if(d.getMonth() == 2){
+        if(d.getMonth() == 6){
         document.getElementById("pyro").style.visibility="visable";
+        }else{
+            document.getElementById("pyro").style.visibility="hidden";
         }
             //-------------------easter-------------------\\
         if(d.getMonth() == 2){
